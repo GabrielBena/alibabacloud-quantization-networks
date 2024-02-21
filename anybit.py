8b608217ebc950f2db1c9e53d258bd9f12d38945
Gabriel Béna <gabriel.bena@spinncloud.com>
2024-02-21 15:19:19 +0100

    mnist example

--- anybit.py
+++ anybit.py
@@ -12,6 +12,7 @@ import math
 import numpy
 import pdb
 
+
 def sigmoid_t(x, b=0, t=1):
     """
     The sigmoid function with T for soft quantization function.
@@ -26,14 +27,16 @@ def sigmoid_t(x, b=0, t=1):
     temp = torch.clamp(temp, min=-10.0, max=10.0)
     return 1.0 / (1.0 + torch.exp(temp))
 
+
 def step(x, bias):
-    """ 
+    """
     The step function for ideal quantization function in test stage.
     """
-    y = torch.zeros_like(x) 
-    mask = torch.gt(x - bias,  0.0)
+    y = torch.zeros_like(x)
+    mask = torch.gt(x - bias, 0.0)
     y[mask] = 1.0
     return y
+
 
 class QuaOp(object):
     """
@@ -43,11 +46,12 @@ class QuaOp(object):
         QW_biases (list): the bias of quantization function.
                           QW_biases is a list with m*n shape, m is the number of layers,
                           n is the number of sigmoid_t.
-        QW_values (list): the list of quantization values, 
+        QW_values (list): the list of quantization values,
                           such as [-1, 0, 1], [-2, -1, 0, 1, 2].
     Returns:
         Quantized model.
     """
+
     def __init__(self, model, QW_biases, QW_values=[]):
         # Count the number of Conv2d and Linear
         count_targets = 0
@@ -57,9 +61,11 @@ class QuaOp(object):
         # Omit the first conv layer and the last linear layer
         start_range = 1
         end_range = count_targets - 2
-        self.bin_range = numpy.linspace(start_range,
-                end_range, end_range-start_range+1)\
-                        .astype('int').tolist()
+        self.bin_range = (
+            numpy.linspace(start_range, end_range, end_range - start_range + 1)
+            .astype("int")
+            .tolist()
+        )
         self.num_of_params = len(self.bin_range)
         self.saved_params = []
         self.target_params = []
@@ -73,39 +79,39 @@ class QuaOp(object):
                     self.saved_params.append(tmp)
                     self.target_modules.append(m.weight)
 
-        print('target_modules number: ', len(self.target_modules))
+        print("target_modules number: ", len(self.target_modules))
         self.QW_biases = QW_biases
         self.QW_values = QW_values
-        # the number of sigmoid_t 
+        # the number of sigmoid_t
         self.n = len(self.QW_values) - 1
         self.threshold = self.QW_values[-1] * 5 / 4.0
         # the gap between two quantization values
         self.scales = []
-        offset = 0.
+        offset = 0.0
         for i in range(self.n):
             gap = self.QW_values[i + 1] - self.QW_values[i]
             self.scales.append(gap)
             offset += gap
-        self.offset = offset / 2.
+        self.offset = offset / 2.0
 
     def forward(self, x, T, quan_bias, train=True):
         if train:
-            y = sigmoid_t(x, b=quan_bias[0], t=T)*self.scales[0]
+            y = sigmoid_t(x, b=quan_bias[0], t=T) * self.scales[0]
             for j in range(1, self.n):
-                y += sigmoid_t(x, b=quan_bias[j], t=T)*self.scales[j]
+                y += sigmoid_t(x, b=quan_bias[j], t=T) * self.scales[j]
         else:
-            y = step(x, bias=quan_bias[0])*self.scales[0] 
+            y = step(x, bias=quan_bias[0]) * self.scales[0]
             for j in range(1, self.n):
-                y += step(x, bias=quan_bias[j])*self.scales[j]
+                y += step(x, bias=quan_bias[j]) * self.scales[j]
         y = y - self.offset
 
         return y
 
     def backward(self, x, T, quan_bias):
-        y_1 = sigmoid_t(x, b=quan_bias[0], t=T)*self.scales[0]
+        y_1 = sigmoid_t(x, b=quan_bias[0], t=T) * self.scales[0]
         y_grad = (y_1.mul(self.scales[0] - y_1)).div(self.scales[0])
         for j in range(1, self.n):
-            y_temp = sigmoid_t(x, b=quan_bias[j], t=T)*self.scales[j]
+            y_temp = sigmoid_t(x, b=quan_bias[j], t=T) * self.scales[j]
             y_grad += (y_temp.mul(self.scales[j] - y_temp)).div(self.scales[j])
 
         return y_grad
@@ -114,11 +120,11 @@ class QuaOp(object):
         """
         The operation of network quantization.
         Args:
-            T: the temperature, a single number. 
+            T: the temperature, a single number.
             alpha: the scale factor of the output, a list.
-            beta: the scale factor of the input, a list. 
+            beta: the scale factor of the input, a list.
             init: a flag represents the first loading of the quantization function.
-            train_phase: a flag represents the quantization 
+            train_phase: a flag represents the quantization
                   operation in the training stage.
         """
         self.save_params()
@@ -135,32 +141,32 @@ class QuaOp(object):
         for index in range(self.num_of_params):
             self.target_modules[index].data.copy_(self.saved_params[index])
 
-
     def quantizeConvParams(self, T, alpha, beta, init, train_phase):
         """
         quantize the parameters in forward
         """
-        T = (T > 2000)*2000 + (T <= 2000)*T
+        T = (T > 2000) * 2000 + (T <= 2000) * T
         for index in range(self.num_of_params):
             if init:
-                beta[index].data = torch.Tensor([self.threshold / self.target_modules[index].data.abs().max()]).cuda()
+                beta[index].data = torch.Tensor(
+                    [self.threshold / self.target_modules[index].data.abs().max()]
+                ).cuda()
                 alpha[index].data = torch.reciprocal(beta[index].data)
             # scale w
             x = self.target_modules[index].data.mul(beta[index].data)
-            
-            y = self.forward(x, T, self.QW_biases[index], train=train_phase)
-            #scale w^hat
-            self.target_modules[index].data = y.mul(alpha[index].data)
 
+            y = self.forward(x, T, self.QW_biases[index], train=train_phase)
+            # scale w^hat
+            self.target_modules[index].data = y.mul(alpha[index].data)
 
     def updateQuaGradWeight(self, T, alpha, beta, init):
         """
         Calculate the gradients of all the parameters.
         The gradients of model parameters are saved in the [Variable].grad.data.
         Args:
-            T: the temperature, a single number. 
+            T: the temperature, a single number.
             alpha: the scale factor of the output, a list.
-            beta: the scale factor of the input, a list. 
+            beta: the scale factor of the input, a list.
             init: a flag represents the first loading of the quantization function.
         Returns:
             alpha_grad: the gradient of alpha.
@@ -168,25 +174,35 @@ class QuaOp(object):
         """
         beta_grad = [0.0] * len(beta)
         alpha_grad = [0.0] * len(alpha)
-        T = (T > 2000)*2000 + (T <= 2000)*T 
+        T = (T > 2000) * 2000 + (T <= 2000) * T
         for index in range(self.num_of_params):
             if init:
-                beta[index].data = torch.Tensor([self.threshold / self.target_modules[index].data.abs().max()]).cuda()
+                beta[index].data = torch.Tensor(
+                    [self.threshold / self.target_modules[index].data.abs().max()]
+                ).cuda()
                 alpha[index].data = torch.reciprocal(beta[index].data)
             x = self.target_modules[index].data.mul(beta[index].data)
 
             # set T = 1 when train binary model
             y_grad = self.backward(x, 1, self.QW_biases[index]).mul(T)
             # set T = T when train the other quantization model
-            #y_grad = self.backward(x, T, self.QW_biases[index]).mul(T)
-            
-        
-            beta_grad[index] = y_grad.mul(self.target_modules[index].data).mul(alpha[index].data).\
-                               mul(self.target_modules[index].grad.data).sum()
-            alpha_grad[index] = self.forward(x, T, self.QW_biases[index]).\
-                                mul(self.target_modules[index].grad.data).sum()
+            # y_grad = self.backward(x, T, self.QW_biases[index]).mul(T)
 
-            self.target_modules[index].grad.data = y_grad.mul(beta[index].data).mul(alpha[index].data).\
-                                                   mul(self.target_modules[index].grad.data)
+            beta_grad[index] = (
+                y_grad.mul(self.target_modules[index].data)
+                .mul(alpha[index].data)
+                .mul(self.target_modules[index].grad.data)
+                .sum()
+            )
+            alpha_grad[index] = (
+                self.forward(x, T, self.QW_biases[index])
+                .mul(self.target_modules[index].grad.data)
+                .sum()
+            )
+
+            self.target_modules[index].grad.data = (
+                y_grad.mul(beta[index].data)
+                .mul(alpha[index].data)
+                .mul(self.target_modules[index].grad.data)
+            )
         return alpha_grad, beta_grad
-
--- models/quantization.py
+++ models/quantization.py
@@ -13,14 +13,15 @@ from torch.autograd import Variable
 import numpy as np
 import pdb
 
+
 class SigmoidT(torch.autograd.Function):
-    """ sigmoid with temperature T for training
-        we need the gradients for input and bias
-        for customization of function, refer to https://pytorch.org/docs/stable/notes/extending.html
+    """sigmoid with temperature T for training
+    we need the gradients for input and bias
+    for customization of function, refer to https://pytorch.org/docs/stable/notes/extending.html
     """
 
     @staticmethod
-    def forward(self, input, scales, n, b, T):        
+    def forward(self, input, scales, n, b, T):
         self.save_for_backward(input)
         self.T = T
         self.b = b
@@ -31,39 +32,41 @@ class SigmoidT(torch.autograd.Function):
         output = self.scales[0] / (1.0 + torch.exp(-buf))
         for k in range(1, self.n):
             buf = torch.clamp(self.T * (input - self.b[k]), min=-10.0, max=10.0)
-            output += self.scales[k] / (1.0 + torch.exp(-buf)) 
+            output += self.scales[k] / (1.0 + torch.exp(-buf))
         return output
 
     @staticmethod
     def backward(self, grad_output):
         # set T = 1 when train binary model in the backward.
-        #self.T = 1
-        input, = self.saved_tensors
+        # self.T = 1
+        (input,) = self.saved_tensors
         b_buf = torch.clamp(self.T * (input - self.b[0]), min=-10.0, max=10.0)
         b_output = self.scales[0] / (1.0 + torch.exp(-b_buf))
         temp = b_output * (1 - b_output) * self.T
-        for j in range(1, self.n):        
+        for j in range(1, self.n):
             b_buf = torch.clamp(self.T * (input - self.b[j]), min=-10.0, max=10.0)
             b_output = self.scales[j] / (1.0 + torch.exp(-b_buf))
             temp += b_output * (1 - b_output) * self.T
-        grad_input = Variable(temp) * grad_output      
+        grad_input = Variable(temp) * grad_output
         # corresponding to grad_input
         return grad_input, None, None, None, None
 
+
 sigmoidT = SigmoidT.apply
 
+
 def step(x, b):
-    """ 
+    """
     The step function for ideal quantization function in test stage.
     """
     y = torch.zeros_like(x)
-    mask = torch.gt(x - b,  0.0)
+    mask = torch.gt(x - b, 0.0)
     y[mask] = 1.0
     return y
 
 
 class Quantization(nn.Module):
-    """ Quantization Activation
+    """Quantization Activation
     Args:
        quant_values: the target quantized values, like [-4, -2, -1, 0, 1 , 2, 4]
        quan_bias and init_beta: the data for initialization of quantization parameters (biases, beta)
@@ -78,26 +81,38 @@ class Quantization(nn.Module):
         - for activations, just pending this module to the activations when build the graph
     """
 
-    def __init__(self, quant_values=[-1, 0, 1], quan_bias=[0], init_beta=0.0):
+    def __init__(
+        self,
+        quant_values=[-1, 0, 1],
+        quan_bias=[0],
+        init_beta=0.0,
+        device=(
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        ),
+    ):
         super(Quantization, self).__init__()
         """register_parameter: params w/ grad, and need to be learned
             register_buffer: params w/o grad, do not need to be learned
             example shown in: https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/batchnorm.py
         """
         self.values = quant_values
+        self.device = device
+
         # number of sigmoids
-        self.n = len(self.values) - 1 
+        self.n = len(self.values) - 1
         self.alpha = Parameter(torch.Tensor([1]))
         self.beta = Parameter(torch.Tensor([1]))
-        self.register_buffer('biases', torch.zeros(self.n))
-        self.register_buffer('scales', torch.zeros(self.n))
-          
+        self.register_buffer("biases", torch.zeros(self.n))
+        self.register_buffer("scales", torch.zeros(self.n))
+
         boundary = np.array(quan_bias)
         self.init_scale_and_offset()
         self.bias_inited = False
         self.alpha_beta_inited = False
         self.init_biases(boundary)
         self.init_alpha_and_beta(init_beta)
+
+        self.T = 1000
 
     def init_scale_and_offset(self):
         """
@@ -107,16 +122,18 @@ class Quantization(nn.Module):
             gap = self.values[i + 1] - self.values[i]
             self.scales[i] = gap
 
+        self.offset = 0.5 * np.array(self.scales).sum()
+
     def init_biases(self, init_data):
         """
         Initialize the bias of quantization function.
         init_data in numpy format.
-        """                    
+        """
         # activations initialization (obtained offline)
         assert init_data.size == self.n
         self.biases.copy_(torch.from_numpy(init_data))
         self.bias_inited = True
-        #print('baises inited!!!')
+        # print('baises inited!!!')
 
     def init_alpha_and_beta(self, init_beta):
         """
@@ -124,22 +141,22 @@ class Quantization(nn.Module):
         init_data in numpy format.
         """
         # activations initialization (obtained offline)
-        self.beta.data = torch.Tensor([init_beta]).cuda()
+        self.beta.data = torch.Tensor([init_beta]).to(self.device)
         self.alpha.data = torch.reciprocal(self.beta.data)
         self.alpha_beta_inited = True
 
-    def forward(self, input, T=1):
-        assert self.bias_inited 
+    def forward(self, input):
+        # print(input)
+        assert self.bias_inited
         input = input.mul(self.beta)
         if self.training:
             assert self.alpha_beta_inited
-            output = sigmoidT(input, self.scales, self.n, self.biases, T)
+            output = sigmoidT(input, self.scales, self.n, self.biases, self.T)
         else:
-            output = step(input, b=self.biases[0])*self.scales[0]       
+            output = step(input, b=self.biases[0]) * self.scales[0]
             for i in range(1, self.n):
-                output += step(input, b=self.biases[i])*self.scales[i]
-            
-        output = output.mul(self.alpha)
+                step_res = step(input, b=self.biases[i]) * self.scales[i]
+                output += step_res
+
+        output = output.mul(self.alpha) - self.offset
         return output
-
-
